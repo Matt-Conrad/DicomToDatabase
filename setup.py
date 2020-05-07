ec7778ad5f22c918b1dbab2e48f7b9471ae0a2c3
Matt-Conrad <mattgrayconrad@gmail.com>
2020-05-07 16:21:32 -0400

    Restructured and added files to create pip package to be uploaded to PyPI so that the library can be used more easily than through git submodule

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metadata_to_db",
+    version="0.0.1",
+    author="Matt-Conrad",
+    author_email="mattgrayconrad@gmail.com",
+    description="Library for storing image metadata from a directory of images to a PostgreSQL DB.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Matt-Conrad/DicomToDatabase",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
